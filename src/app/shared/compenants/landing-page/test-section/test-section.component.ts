cb14371107bc0b2a49520accfa089035de9f2dbf
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-24 21:15:09 +0100

    create compenants for landing page

---
+++ src/app/shared/compenants/landing-page/test-section/test-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-test-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './test-section.component.html',
+  styleUrls: ['./test-section.component.scss']
+})
+export class TestSectionComponent {
+
+}
