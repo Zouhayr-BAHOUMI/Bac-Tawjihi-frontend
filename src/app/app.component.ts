904d705555811ed437ee3cca920afe1166c23207
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-24 22:35:11 +0100

    ajouter compenant du ecoles services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./auth/login/login.component";
+import { EcolesSectionComponent } from "./shared/compenants/landing-page/ecoles-section/ecoles-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent],
+  imports: [CommonModule, RouterOutlet, LoginComponent, EcolesSectionComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
---
+++ src/app/shared/compenants/landing-page/ecoles-section/ecoles-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ecoles-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ecoles-section.component.html',
+  styleUrls: ['./ecoles-section.component.scss']
+})
+export class EcolesSectionComponent {
+
+}
