8b2e1fc7b15d08a83dda4c04f9fcd4b347ddfb9f
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-25 11:42:56 +0100

    faire main layout for landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./auth/login/login.component";
 import { EcolesSectionComponent } from "./shared/compenants/landing-page/ecoles-section/ecoles-section.component";
+import { WarraperComponent } from "./shared/compenants/landing-page/warraper/warraper.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent, EcolesSectionComponent],
+  imports: [CommonModule, RouterOutlet, LoginComponent, EcolesSectionComponent, WarraperComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
