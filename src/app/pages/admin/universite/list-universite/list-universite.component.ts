255d10331efcee7648ca53d1a381fcec430addc5
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-29 12:23:25 +0100

    implimente the get all universities

--- src/app/pages/admin/universite/list-universite/list-universite.component.ts
+++ src/app/pages/admin/universite/list-universite/list-universite.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Universite } from 'src/app/interfaces/universite';
+import { UniversiteService } from 'src/app/shared/services/universite.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-list-universite',
@@ -8,6 +11,33 @@ import { CommonModule } from '@angular/common';
   templateUrl: './list-universite.component.html',
   styleUrls: ['./list-universite.component.scss']
 })
-export class ListUniversiteComponent {
+export class ListUniversiteComponent implements OnInit {
 
+  universites: Universite[] = [];
+  universiteToDelete: Universite | null = null;
+  showModal = false;
+
+  constructor(private universiteService: UniversiteService) { }
+
+  ngOnInit() {
+    this.getUniversites();
+  }
+
+
+  public getUniversites(): void {
+    this.universiteService.getUniversites().subscribe(
+      (response: Universite[]) => {
+        console.log(response);
+        this.universites = response;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+      }
+    );
+  }
+
+  openDeleteModal(universite: Universite): void {
+    this.universiteToDelete = universite;
+    this.showModal = true;
+  }
 }
--- src/app/shared/services/universite.service.ts
+++ src/app/shared/services/universite.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Universite } from 'src/app/interfaces/universite';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UniversiteService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8082/admin/gestion-universities';
+
+  constructor(private http: HttpClient) { }
+
+  public getUniversites(): Observable<Universite[]> {
+    return this.http.get<Universite[]>(`${this.apiUrl}/`);
+  }
 }
