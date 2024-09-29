c750d69a2ea5d48ba9014e1f5e86c494781151ac
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-09-29 10:38:13 +0100

    definir structure du dashboard for admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { WarraperComponent } from './shared/compenants/landing-page/warraper/warraper.component';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: WarraperComponent },
+    { path: 'login', component: LoginComponent, pathMatch: 'full' },
+
+];
