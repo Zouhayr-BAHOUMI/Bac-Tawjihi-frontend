7c1a09b00bfa8448c876629b921e15a220c8fa88
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-10-01 15:11:12 +0100

    faire les compenant pour la gestion des etablissements

---
+++ src/app/pages/admin/etablissement/edit-etablissement/edit-etablissement.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-edit-etablissement',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './edit-etablissement.component.html',
+  styleUrls: ['./edit-etablissement.component.scss']
+})
+export class EditEtablissementComponent {
+
+}
---
+++ src/app/pages/admin/etablissement/list-etablissement/list-etablissement.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-list-etablissement',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list-etablissement.component.html',
+  styleUrls: ['./list-etablissement.component.scss']
+})
+export class ListEtablissementComponent {
+
+}
