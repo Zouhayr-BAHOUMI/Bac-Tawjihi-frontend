e4f5840f887d3dedeadbe0062c2558f1025f2195
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-10-01 15:23:46 +0100

    faire service pour gestion des etablissements

--- src/app/shared/services/etablissement.service.ts
+++ src/app/shared/services/etablissement.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Etablissement } from 'src/app/interfaces/etablissement';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EtablissementService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8082/admin/gestion-etablissements';
+
+  constructor(private http: HttpClient) { }
+
+
+  public getAllEtablissements(): Observable<Etablissement[]> {
+    return this.http.get<Etablissement[]>(`${this.apiUrl}/`);
+  }
+
+
+  public addEtablissement(etablissementDto: EtablissementDto, idUniversite: number): Observable<EtablissementDto> {
+    return this.http.post<EtablissementDto>(`${this.apiUrl}/add/${idUniversite}`, etablissementDto);
+  }
+
+
+  public getEtablissementById(idEtablissement: number): Observable<Etablissement> {
+    return this.http.get<Etablissement>(`${this.apiUrl}/idEtablissement?idEtablissement=${idEtablissement}`);
+  }
+
+
+  public updateEtablissement(idEtablissement: number, etablissementDto: EtablissementDto): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/update/${idEtablissement}`, etablissementDto, {
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
+
+
+  public deleteEtablissement(idEtablissement: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/${idEtablissement}`);
+  }
 }
