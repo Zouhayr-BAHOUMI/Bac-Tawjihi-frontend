23d4603e4bdf71eafe8c9c94456796e33b701050
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-10-01 15:25:12 +0100

    faire dto pour etablissement

--- src/app/dto/etablissement-dto.ts
+++ src/app/dto/etablissement-dto.ts
@@ -1,2 +1,23 @@
+import { Province } from "../enums/province";
+import { Region } from "../enums/region";
+import { TypeEtablissement } from "../enums/type-etablissement";
+import { Ville } from "../enums/ville";
+import { Universite } from "../interfaces/universite";
+
 export interface EtablissementDto {
+
+    id: number;
+    nomEtablissement: string;
+    imageUrl?: string;  
+    localisation?: string;  
+    condition: string;
+    proceduresCandidature?: string;  
+    calendrier?: string;  
+    typeEtablissement: TypeEtablissement;
+    universite?: Universite; 
+    adresse: {
+        region: Region;
+        province: Province;
+        ville: Ville;
+    };
 }
