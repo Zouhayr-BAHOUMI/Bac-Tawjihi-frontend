6c02882c354a686010b1cd5f9511ea726d90027e
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-10-04 11:27:03 +0100

    faire service pour la gestion packs

--- src/app/shared/services/pack.service.ts
+++ src/app/shared/services/pack.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Pack } from 'src/app/interfaces/pack';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PackService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8082/admin/gestion-packs';
+
+  constructor(private http: HttpClient) { }
+
+  public getPacks(): Observable<Pack[]> {
+    return this.http.get<Pack[]>(`${this.apiUrl}/`);
+  }
+
+  public addPack(pack: Pack): Observable<string> {
+    return this.http.post<string>(`${this.apiUrl}/add`, pack, {
+      responseType: 'text' as 'json'
+    });
+  }
+
+  public getPackById(idPack: number): Observable<Pack> {
+    return this.http.get<Pack>(`${this.apiUrl}/idPanne?idPack=${idPack}`);
+  }
+
+  public updatePack(idPack: number, pack: Pack): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/update/${idPack}`, pack, {
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
+
+  public deletePack(idPack: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/delete/${idPack}`);
+  }
 }
