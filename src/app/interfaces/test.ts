174608e96a7fb6e201a629e00190bf2a1dcfcec2
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-10-05 16:02:10 +0100

    definir interface d'un test

--- src/app/interfaces/test.ts
+++ src/app/interfaces/test.ts
@@ -1,2 +1,10 @@
+import { Etudiant } from "./etudiant";
+
 export interface Test {
+
+    id: number;
+    titre: string;
+    description: string;
+    duree: string;
+    etudiant: Etudiant | number;
 }
--- src/app/pages/admin/test/list-test/list-test.component.ts
+++ src/app/pages/admin/test/list-test/list-test.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-list-test',
   standalone: true,
-  imports: [CommonModule,ButtonModule, CardModule],
+  imports: [CommonModule,ButtonModule, CardModule, RouterModule],
   templateUrl: './list-test.component.html',
   styleUrls: ['./list-test.component.scss']
 })
