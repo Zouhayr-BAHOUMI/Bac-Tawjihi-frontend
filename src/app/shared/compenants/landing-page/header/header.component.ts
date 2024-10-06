6590e1bafe59cea9e6306598a5e264c97cce26ac
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-10-06 22:50:17 +0100

    display packs exist in page home

--- src/app/shared/compenants/landing-page/header/header.component.ts
+++ src/app/shared/compenants/landing-page/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from 'src/app/core/auth.service';
+import { Etudiant } from 'src/app/interfaces/etudiant';
+import { EtudiantService } from 'src/app/shared/services/etudiant.service';
 
 @Component({
   selector: 'app-header',
@@ -14,16 +16,29 @@ export class HeaderComponent implements OnInit {
 
   isLoggedIn: boolean = false;
   userName: string | null = null;
+  etudiant: Etudiant | null = null;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService, 
+    private etudiantService : EtudiantService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.isLoggedIn = this.authService.isLoggedIn();
     if (this.isLoggedIn) {
-      this.userName = this.authService.getCurrentUserName();
+      // Fetch the student's profile
+      this.etudiantService.getEtudiantProfile().subscribe(
+        (response: Etudiant) => {
+          this.etudiant = response;
+        },
+        error => {
+          console.error('Error fetching student profile:', error);
+        }
+      );
     }
   }
-
+  
   logout() {
     this.authService.logout();
     this.isLoggedIn = false;
--- src/app/shared/compenants/landing-page/pricing/pricing.component.ts
+++ src/app/shared/compenants/landing-page/pricing/pricing.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Pack } from 'src/app/interfaces/pack';
+import { PackService } from 'src/app/shared/services/pack.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-pricing',
@@ -8,6 +11,26 @@ import { CommonModule } from '@angular/common';
   templateUrl: './pricing.component.html',
   styleUrls: ['./pricing.component.scss']
 })
-export class PricingComponent {
+export class PricingComponent implements OnInit{
+
+  packs: Pack[] = [];
+
+  constructor(private packService: PackService) {}
+
+  ngOnInit() {
+    this.getPacks();
+  }
+
+  public getPacks(): void {
+    this.packService.getPacksWithFilieres().subscribe(
+      (response: Pack[]) => {
+        console.log(response);
+        this.packs = response;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+      }
+    );
+  }
 
 }
