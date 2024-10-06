a53836b4fd8b2a8f22f495f3dc15c91bba02e4c6
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-10-06 13:24:38 +0100

    faire logic pour afficher les questions

--- src/app/pages/admin/questions/list-question/list-question.component.ts
+++ src/app/pages/admin/questions/list-question/list-question.component.ts
@@ -1,13 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Question } from 'src/app/interfaces/question';
+import { QuestionService } from 'src/app/shared/services/question.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-list-question',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DialogModule, ButtonModule, RouterModule],
   templateUrl: './list-question.component.html',
   styleUrls: ['./list-question.component.scss']
 })
-export class ListQuestionComponent {
+export class ListQuestionComponent implements OnInit {
+
+  questions: Question[] = [];
+  questionToDelete: Question | null = null;
+  showModal = false;
+
+  constructor(private questionService: QuestionService) {}
+
+  ngOnInit() {
+    this.getQuestions();
+  }
+
+  public getQuestions(): void {
+    this.questionService.getQuestions().subscribe(
+      (response: Question[]) => {
+        console.log(response);
+        this.questions = response;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+      }
+    );
+  }
+
+  openDeleteModal(question: Question): void {
+    this.questionToDelete = question;
+    this.showModal = true;
+  }
+
+  cancelDelete(): void {
+    this.showModal = false;
+    this.questionToDelete = null;
+  }
+
+  confirmDelete(): void {
+    if (this.questionToDelete) {
+      
+    }
+  }
+
+  deleteQuestion(id: number): void {
+    this.questionService.deleteQuestion(id).subscribe(() => {
+      this.getQuestions(); 
+      this.cancelDelete(); 
+    });
+  }
 
 }
