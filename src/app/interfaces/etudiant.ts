5c6e6386a778ac62337dc4ae5a86d3bcf7b95e36
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-10-06 16:39:10 +0100

    definir interface etudiant avec les enums needed

--- src/app/interfaces/etudiant.ts
+++ src/app/interfaces/etudiant.ts
@@ -1,4 +1,21 @@
+import { Sexe } from "../enums/sexe";
+import { Specialite } from "../enums/specialite";
+import { Adresse } from "./adresse";
 import { User } from "./user";
 
 export interface Etudiant extends User{
+
+    id: number;
+    nom: string;
+    prenom: string;
+    cin: string;
+    imageUrl: string;
+    sexe: Sexe; 
+    dateNaissence: string; 
+    lieuNaissence: string;
+    tele: string;
+    specialite: Specialite; 
+    codeMassar: string;
+    adresse?: Adresse;
+
 }
